c95c4fde79f6ee7668ac21b160cc86f53cd76e33
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-17 09:42:27 +0900

    testablescheduler class 생성

---
+++ src/test/java/com/sds/cleancode/restaurant/TestableBookingScheduler.java
@@ -0,0 +1,17 @@
+package com.sds.cleancode.restaurant;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestableBookingScheduler extends BookingScheduler {
+    private String dateTime;
+    public TestableBookingScheduler(int capacityPerHour, String dateTime) {
+        super(capacityPerHour);
+        this.dateTime = dateTime;
+    }
+    @Override
+    public LocalDateTime getNow() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        return LocalDateTime.parse(dateTime, format);
+    }
+}
