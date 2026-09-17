21608803c97834c65e58d5baca69c4ffe1c7205a
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-17 09:17:14 +0900

    정시에만 예약가능 TC

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -3,14 +3,42 @@ package com.sds.cleancode.restaurant;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class BookingSchedulerTest {
 
     @Test
     public void 예약은_정시에만_가능하다_정시가_아닌경우_예약불가() {
+        //arrange
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        LocalDateTime notOnTheHour = LocalDateTime.parse("2021/03/26 09:05", dateTimeFormatter);
+        Customer customer = new Customer("Fake name", "010-1234-5678");
+        Schedule schedule = new Schedule(notOnTheHour, 1, customer);
+        BookingScheduler bookingScheduler = new BookingScheduler(3);
+        //act
+        assertThatThrownBy(() -> {
+            bookingScheduler.addSchedule(schedule);
+        }).isInstanceOf(RuntimeException.class);
+    //assert
+    //expected runtime exception
     }
 
     @Test
     public void 예약은_정시에만_가능하다_정시인_경우_예약가능() {
+    //arrange
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        LocalDateTime onTheHour = LocalDateTime.parse("2021/03/26 09:00", dateTimeFormatter);
+        Customer customer = new Customer("Fake name", "010-1234-5678");
+        Schedule schedule = new Schedule(onTheHour, 1, customer);
+        BookingScheduler bookingScheduler = new BookingScheduler(3);
+    //act
+        bookingScheduler.addSchedule(schedule);
+    //assert
+        assertThat(bookingScheduler.hasSchedule(schedule)).isEqualTo(true);
     }
 
     @Test
